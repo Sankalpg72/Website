5ffde516f9d17b26c1222a388981f553fb810655
Sankalpg72 <116297547+Sankalpg72@users.noreply.github.com>
2024-08-22 18:38:07 +0000

    CSS styling and html basic

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('home.html')
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",debug=True)
+  app.run(host = "0.0.0.0",debug = True)
+
+#In Flask, the default convention for locating HTML templates is to look in a folder named templates. This is built into Flask's template rendering system. Here’s why using template instead of templates didn’t work:
+
+# Flask Convention: Flask expects the directory where templates are stored to be named templates. This is a convention followed by the Flask framework. If the directory name is different, Flask won’t be able to locate the templates.
+
